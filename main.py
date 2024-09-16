6d00ee5b7303c27dcf38a1cb83558e5c37b54301
stefantobiasiewicz <stefantobiasiewicz@gmail.com>
2024-09-16 21:44:45 +0200

    second

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 import os
 from PIL import Image
 
-RUN_RPI = os.getenv('RUN_RPI') == True
+RUN_RPI = os.getenv('RUN_RPI', '').lower() in ['1', 'true', 'yes']
 
 if RUN_RPI:
     from waveshare_epd import epd2in13b_V4
